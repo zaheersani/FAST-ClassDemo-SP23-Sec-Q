4ea37e940fe35e7429a305c9a0817d907771af98
zaheersani <sani.zaheer@gmail.com>
2023-03-06 11:37:45 +0500

    jQuery with JSONPlaceholder API - TASKS

---
+++ JSArrays.js
@@ -0,0 +1,13 @@
+var arr = [3, 5, "six", true, [99, 99], { key: 'value' }]
+
+arr[10] = "10th Index"
+
+// for (let index = 0; index < arr.length; index++)
+//     console.log(arr[index])
+
+console.log(arr[5].key)
+
+// arr.forEach(element => {
+//     console.log(element)
+// });
+    
